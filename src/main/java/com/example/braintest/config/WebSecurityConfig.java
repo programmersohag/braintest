c7cb0db8f3a1f98475a4c754633046540d2c7039
Sohag Hossain <sohaginfo1@gmail.com>
2021-07-08 00:44:44 +0600

    logout done

--- src/main/java/com/example/braintest/config/WebSecurityConfig.java
+++ src/main/java/com/example/braintest/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -58,8 +59,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/homepage", true)
                 .failureUrl("/login?error=true")
                 .and()
-                .logout();
-        //      .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+                .logout()
+                .logoutSuccessUrl("/homepage")
+              .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 //        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
